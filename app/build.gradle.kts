0a749146ee5c969fb7ae5dae8248a021930c34b7
amnesia <anoN@example.com>
2026-09-05 13:40:15 -0400

    Fix anilist auth issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 157
-        versionName = "1.21.12"
+        versionCode = 158
+        versionName = "1.21.13"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
